630d3577b23b333e81a5851442fc023ce932a1cf
Admin <admin@alweb31.site>
2021-05-25 16:48:45 +0300

    Добавили модель и шаблон

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title',)
+
+
+admin.site.register(News, NewsAdmin)
--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Контент')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    photo = models.ImageField(blank=True, upload_to='photos/%Y/%m/%d/', verbose_name='Изображение')
+    is_published = models.BooleanField(default=True, verbose_name='Статус публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
